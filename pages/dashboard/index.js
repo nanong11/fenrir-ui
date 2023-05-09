740a62d06d2b2d6e328619e707e8b776ed02c211
nanong11 <marcallen.nanong11@gmail.com>
2023-05-09 10:02:38 +0800

    On going initial development

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import DashboardStyled from './dashboard.styles'
-import { Layout, Menu, theme, Typography, Button } from 'antd';
+import { Layout, Menu, theme, Typography, Button, Avatar, Space } from 'antd';
 // import { useSelector } from 'react-redux';
-import { MenuFoldOutlined, MenuUnfoldOutlined, GroupOutlined, FileSearchOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, GroupOutlined, FileSearchOutlined, UsergroupAddOutlined, UserOutlined, AntDesignOutlined, ProfileOutlined } from '@ant-design/icons';
 // import * as pallete from '@/styles/variables';
 
 const { Header, Content, Sider } = Layout;
@@ -17,18 +17,34 @@ export default function Dashboard() {
 
   const [collapse, setCollapse] = useState(true)
   const [broken, setBroken] = useState(null)
-  const [selectedMenu, setSelectedMenu] = useState(null)
+  const [selectedMenu, setSelectedMenu] = useState('PROFILE')
+  const [hideContent, setHideContent] = useState(false)
 
   const sideMenuItems = [
+    {
+      key: 'PROFILE',
+      icon: <ProfileOutlined />,
+      label: `PROFILE`,
+      allowed: 'any'
+    },
     {
       icon: <GroupOutlined />,
       label: `CHANNELS`,
       allowed: 'any',
-      children: [{
-        label: 'Attendance'
-      }].map((item, index) => {
-        return {
+      children: [
+        {
+          key: 'Add Channel',
+          label: 'Add Channel',
+          allowed: 'admin'
+        },
+        {
           key: 'Attendance',
+          label: 'Attendance',
+          allowed: 'any'
+        },
+      ].map((item, index) => {
+        return {
+          key: item.key,
           label: item.label
         }
       })
@@ -50,6 +66,12 @@ export default function Dashboard() {
   const handleMenuItemOnClick = (value) => {
     setSelectedMenu(value.key)
     setCollapse(!collapse)
+    setHideContent(false)
+  }
+
+  const handleMenuBtnOnClick = () => {
+    setCollapse(!collapse)
+    setHideContent(collapse ? true : false)
   }
 
   return (
@@ -60,7 +82,7 @@ export default function Dashboard() {
         collapsible
         breakpoint="sm"
         collapsedWidth={broken && collapse ? '0' : '80px'}
-        width={broken ? '100vw' : '200px'}
+        width={broken ? '85vw' : '200px'}
         collapsed={broken ? collapse : false}
         onBreakpoint={(broken) => {
           setBroken(broken)
@@ -71,9 +93,21 @@ export default function Dashboard() {
         style={{
           background: colorPrimaryBg,
         }}
-        >
-          <div className='logo'>Logo</div>
+        > 
+          <Space
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: broken ? '85vw' : '200px',
+            height: '64px',
+            margin: '0 0 10px 0',
+          }}
+          >
+            <Avatar shape="square" size="large" icon={<UserOutlined />} />
+          </Space>
           <Menu
+          defaultSelectedKeys={['PROFILE']}
           mode="inline"
           items={sideMenuItems.map(
             (item, index) => ({
@@ -93,8 +127,11 @@ export default function Dashboard() {
         <Layout>
           <Header
             style={{
-              padding: 0,
+              padding: '0 10px 0 0',
               background: colorBgContainer,
+              display: 'flex',
+              justifyContent: broken ? 'space-between' : 'flex-end',
+              columnGap: '20px'
             }}
           >
             {
@@ -102,7 +139,7 @@ export default function Dashboard() {
               <Button
               type="text"
               icon={collapse ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapse(!collapse)}
+              onClick={handleMenuBtnOnClick}
               style={{
                 fontSize: '1.5rem',
                 width: 64,
@@ -112,22 +149,32 @@ export default function Dashboard() {
               }}
               /> : null
             }
+
+            <Space align='center'>
+              <Avatar
+                icon={<AntDesignOutlined />}
+              />
+            </Space>
           </Header>
           
           <Content
             style={{
               margin: '10px',
             }}
-          >
-            <div
+          > 
+            {
+              hideContent ? null :
+              <div
               style={{
                 padding: 24,
                 minHeight: '100%',
                 background: colorBgContainer,
               }}
-            >
-              <Text>{selectedMenu}</Text>
-            </div>
+              >
+                <Text>{selectedMenu}</Text>
+              </div>
+
+            }
           </Content>
         </Layout>
       </Layout>
---
+++ pages/login/login.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+// import * as pallete from '@/styles/variables';
+
+const LoginStyled = styled.div`
+    min-width: 100%;
+    min-height: 100%;
+`;
+
+export default LoginStyled;
